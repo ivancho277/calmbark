088c38162019cc8b8147ba74daff6ff534be61a1
ivancho27 <rouskoi@live.com>
2022-03-19 00:31:21 -0700

    first

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import fetchdogpic from './apis/dogapi'
+import dogIcon from './assets/icons8-dog-100.png'
 
 export default function App() {
+
+  [dogPicture, setDogPicture] = useState();
+  //[buttonClick, setButtonClick] = useState(false);
+  // const initialState = async () => {fetchdogpic().then((response) => {
+  //     response.data.
+  // })
+  useEffect(() => {
+    let ignore = false;
+
+    async function fetchdata() {
+      const dogURL = await fetchdogpic();
+      if (!ignore) {
+        setDogPicture(dogURL);
+      }
+    }
+      fetchData();
+      return () => { ignore = true; }
+    }, [dogPicture]);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>An App for Babe sauce {`${dogPicture}`}</Text>
+      <Button onPress={() => fetchdogpic()} soruce={dogPicture} title="press for puppers" />
+      <Image style={styles.img} source={dogPicture} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: '70%',
+    height: '60%',
+  }
 });
---
+++ apis/dogapi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const randomImgUrl = "https://dog.ceo/api/breeds/image/random";
+
+
+async function fetchdogpic() {
+    return await axios.get(randomImgUrl).then((response) => {
+        console.log('----------------------------------------------------');
+        console.log(response.data.message);
+        return response.data.message;
+    })
+}
+
+export default fetchdogpic;
